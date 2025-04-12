8ae81fb218ec80c5ba10e74b83498289cbe046d8
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 07:00:14 -0600

    Add files via upload

---
+++ valor.py
@@ -0,0 +1,14 @@
+
+# utils/valor.py
+
+def detectar_valor_tenis(partido, analisis):
+    return True
+
+def detectar_valor_mlb(partido, analisis):
+    return True
+
+def detectar_valor_nba(partido, analisis):
+    return True
+
+def detectar_valor_futbol(partido, analisis):
+    return True
