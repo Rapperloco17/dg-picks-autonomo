ef57cb9dfe18ff3413dcf1bf986700fd96f32e49
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 08:51:48 -0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,3 @@
-from .cuotas import obtener_cuota_bet365
-from .cuotas_cache import get_cuota_cached
-
+from utils.cuotas import obtener_cuota_bet365
+from utils.cuotas_cache import get_cuota_cached
 
