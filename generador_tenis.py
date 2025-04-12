342029e4d0901e145bb3dcc7c519d31f64ac5d36
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 11:49:18 -0600

    Update generador_tenis.py

--- generador_tenis.py
+++ generador_tenis.py
@@ -1,5 +1,5 @@
 from utils.sofascore import obtener_partidos_tenis, analizar_rompimientos
-from utils.telegram import log_envío
+from utils.telegram import log_envio
 from utils.valor_cuota import (
     detectar_valor_tenis,
     detectar_valor_mlb,
