cc8ec86f4286100306b024c1977bafb25a8eff5a
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 09:34:51 -0600

    Update valor_cuota.py

--- utils/valor_cuota.py
+++ utils/valor_cuota.py
@@ -1,14 +1,13 @@
 # utils/valor_cuota.py
 
-def detectar_valor_tenis(partido, analisis):
-    return True
+def detectar_valor_tenis(cuota, min_valor=1.50, max_valor=3.50):
+    return min_valor <= cuota <= max_valor
 
-def detectar_valor_mlb(partido, analisis):
-    return True
+def detectar_valor_mlb(cuota, min_valor=1.50, max_valor=3.50):
+    return min_valor <= cuota <= max_valor
 
-def detectar_valor_nba(partido, analisis):
-    return True
+def detectar_valor_nba(cuota, min_valor=1.50, max_valor=3.50):
+    return min_valor <= cuota <= max_valor
 
-def detectar_valor_futbol(partido, analisis):
-    return True
-
+def detectar_valor_futbol(cuota, min_valor=1.50, max_valor=3.50):
+    return min_valor <= cuota <= max_valor
