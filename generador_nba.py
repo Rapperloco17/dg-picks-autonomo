8033ffa71261f64e6e680ebaf59dfeab1121bbea
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 19:21:17 -0600

    Update generador_nba.py

--- generador_nba.py
+++ generador_nba.py
@@ -1,14 +1,14 @@
 from utils.sofascore import obtener_partidos_nba
-from utils.telegram import log_envio
+from utils.telegram import log_envío
 from utils.valor_cuota import detectar_valor_nba
 
 def enviar_picks_nba():
-    print("\n🧠 Inicio de análisis de NBA...")
+    print("📊 Inicio de análisis de NBA...")
     partidos = obtener_partidos_nba()
 
     for partido in partidos:
         if detectar_valor_nba(partido["cuota"]):
             mensaje = f"🏀 Pick NBA\n{partido['equipo_local']} vs {partido['equipo_visitante']}\nCuota: {partido['cuota']}"
-            log_envio(mensaje)
+            log_envío(mensaje)
 
     print("✅ Picks de NBA enviados.")
