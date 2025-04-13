1c58083f12dd339604474d59f994366c92682efb
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 21:21:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
+from run_manual import enviar_pick_manual
 
-import schedule
-import time
-
-from generador_tenis import enviar_picks_tenis
-from generador_mlb import enviar_picks_mlb
-from generador_nba import enviar_picks_nba
-from generador_futbol import enviar_picks_futbol
-from generador_parlay import enviar_parlay_diario
-from generador_reto import enviar_reto_escalera
-from generador_mini_reto import enviar_mini_reto
-
-# Horarios programados (ajustables)
-schedule.every().day.at("10:00").do(enviar_picks_tenis)
-schedule.every().day.at("14:00").do(enviar_picks_mlb)
-schedule.every().day.at("14:00").do(enviar_picks_nba)
-schedule.every().day.at("11:00").do(enviar_picks_futbol)
-schedule.every().day.at("12:00").do(enviar_parlay_diario)
-schedule.every().day.at("13:00").do(enviar_reto_escalera)
-schedule.every().day.at("16:00").do(enviar_mini_reto)
-
-# Bucle continuo del sistema
-while True:
-    schedule.run_pending()
-    time.sleep(30)
+if __name__ == "__main__":
+    enviar_pick_manual()
