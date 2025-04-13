7eb079640800967f6c841bd1f7ba214b161011ac
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 20:47:57 -0600

    Update pick_manual_test.py

--- pick_manual_test.py
+++ pick_manual_test.py
@@ -1,1 +1,7 @@
+from utils.telegram import log_envío
+
+mensaje = "🔥 Test de mensaje desde DG Picks 🔥\nSi ves esto, todo funciona bien 😎"
+log_envío('free', mensaje)
+log_envío('vip', mensaje)
+log_envío('reto', mensaje)
 
