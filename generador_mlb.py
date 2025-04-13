6865f83e3ebf8f441386465db404fcf4c46975af
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 20:06:58 -0600

    Add files via upload

--- generador_mlb.py
+++ generador_mlb.py
@@ -1,14 +1,15 @@
+
 from utils.sofascore import obtener_partidos_mlb
-from utils.telegram import log_envio
+from utils.telegram import log_envío
 from utils.valor_cuota import detectar_valor_mlb
 
 def enviar_picks_mlb():
-    print("⚾ Inicio de análisis de MLB...")
+    print("🧢 Inicio de análisis de MLB...")
     partidos = obtener_partidos_mlb()
 
     for partido in partidos:
         if detectar_valor_mlb(partido["cuota"]):
-            mensaje = f"⚾ Pick MLB\n{partido['equipo_local']} vs {partido['equipo_visitante']}\nCuota: {partido['cuota']}"
-            log_envio(mensaje)
+            mensaje = f"⚾️ Pick MLB\n{partido['equipo_local']} vs {partido['equipo_visitante']}\nCuota: {partido['cuota']}"
+            log_envío("vip", mensaje)
 
     print("✅ Picks de MLB enviados.")
