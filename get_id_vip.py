aa2751ea9ff69667f02d6d88125006bba0e2a5f3
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-12 23:59:58 -0600

    Update get_id_vip.py

--- get_id_vip.py
+++ get_id_vip.py
@@ -1,1 +1,13 @@
+from utils.telegram import TELEGRAM_BOT_TOKEN
+import requests
+
+# Aquí pon el username público del canal VIP (sin @)
+username = "dgpickspro17"
+
+url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getChat?chat_id=@{username}"
+
+response = requests.get(url)
+data = response.json()
+
+print(data)
 
