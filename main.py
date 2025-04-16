c01ff36d50b1d54a8b7835ee02482a3a7d5e6fa1
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-16 10:14:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from generador_mini_reto import enviar_mini_reto_escalera
 import schedule
 import time
 
-# ✅ Horarios programados (hora del servidor Railway -6 GMT México)
+# 🕒 Horarios programados (hora del servidor Railway -6 GMT México)
 
 # 🎾 Tenis a las 22:00
 schedule.every().day.at("22:00").do(enviar_picks_tenis)
 
-# 🔒 Reto Escalera – 5 horas antes del mejor pick
+# 🏆 Reto Escalera - 5 horas antes del mejor pick
 schedule.every().day.at("06:00").do(enviar_reto_escalera)
 
-# 💥 Mini Reto Free (cada 2 semanas)
+# 🏅 Mini Reto Free (cada 2 semanas)
 schedule.every().day.at("09:10").do(enviar_mini_reto_escalera)
 
 print("✅ Sistema DG Picks Automático Iniciado")
