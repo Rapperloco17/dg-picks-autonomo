2a68df8437241522732a7f58cbe5394f9376dff4
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-17 22:28:15 -0600

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,14 @@
 import schedule
 import time
 import os
+
 from soccer_generator import generate_soccer_picks
+from top_matches import generate_top5  # Agregado
 
 def schedule_soccer_analysis():
     schedule.every().day.at("04:30").do(generate_soccer_picks)
-    print("⏰ Análisis de fútbol programado a las 04:30 a.m. (hora local del servidor)")
+    schedule.every().day.at("10:10").do(generate_top5)  # Nuevo
+    print("⚽ Análisis de fútbol programado a las 04:30 a.m. y Top 5 a las 10:10 a.m. (hora local del servidor)")
 
     while True:
         schedule.run_pending()
