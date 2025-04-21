dc6ee231e242b58c59d1c885acacd3a649f12a66
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-21 13:36:53 -0600

    Create btts_valor.py
    Añadir módulo de análisis BTTS con valor

---
+++ historial/btts_valor.py
@@ -0,0 +1,9 @@
+def analizar_btts_local_vs_visitante(local_stats, visitante_stats):
+    goles_local = local_stats.get("goals", {}).get("average", {}).get("home", 0)
+    goles_visitante = visitante_stats.get("goals", {}).get("average", {}).get("away", 0)
+    goles_encajados_local = local_stats.get("goals", {}).get("average", {}).get("against", {}).get("home", 0)
+    goles_encajados_visitante = visitante_stats.get("goals", {}).get("average", {}).get("against", {}).get("away", 0)
+
+    if goles_local >= 1.2 and goles_visitante >= 1.2 and goles_encajados_local >= 1.0 and goles_encajados_visitante >= 1.0:
+        return True
+    return False
