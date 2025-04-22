08b1ffa35953791537ddd367108d3d08afb862c8
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-04-21 21:43:53 -0600

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,5 +1,4 @@
 # utils/telegram.py – Envío automático a Telegram para DG Picks
-
 import requests
 
 # TOKEN del bot y chat IDs oficiales
@@ -7,8 +6,8 @@ BOT_TOKEN = "7520899056:AAHaS2Id5BGa9HlrX6YWJFX6hCnZsADTOFA"
 
 # Canales oficiales
 CHAT_IDS = {
-    "VIP": "-1001285733813",
-    "RETO": "-1002453760512",
+    "VIP": -1001285733813,
+    "RETO": -1002453760512,
     "FREE": "@dgpickspro17",
     "ADMIN": "7450739156"  # usuario administrador
 }
@@ -28,11 +27,12 @@ def enviar_mensaje(mensaje, canal="VIP"):
             "text": mensaje,
             "parse_mode": "HTML"
         }
+
         response = requests.post(url, data=data)
         if not response.ok:
-            print(f"\u26a0\ufe0f Error al enviar mensaje a {canal}: {response.text}")
+            print(f"⚠️ Error al enviar mensaje a {canal}: {response.text}")
         else:
-            print(f"\u2705 Mensaje enviado a {canal}")
+            print(f"✅ Enviado con éxito a canal {canal}")
 
     except Exception as e:
-        print(f"\u26a0\ufe0f Error general al enviar mensaje a Telegram ({canal}):", e)
+        print(f"⚠️ Error general al enviar mensaje a Telegram ({canal}):", e)
