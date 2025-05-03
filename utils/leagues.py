b32293c5dcd7518bd391eeb85f7b105b69caae2b
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-05-02 23:24:20 -0600

    Update leagues.py

--- utils/leagues.py
+++ utils/leagues.py
@@ -1,6 +1,6 @@
 import json
 
 def cargar_ligas_permitidas():
-    with open("utils/leagues_whitelist_ids.json") as f:
+    with open("utils/leagues_whitelist_ids.json", "r") as f:
         data = json.load(f)
-        return data.get("allowed_league_ids", [])
+        return data  # Ya es una lista directamente
