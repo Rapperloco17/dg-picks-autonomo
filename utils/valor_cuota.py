41822a70ec6dd2d360804e0acd52f622cb84fd80
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-05-03 11:12:30 -0600

    Update valor_cuota.py

--- utils/valor_cuota.py
+++ utils/valor_cuota.py
@@ -12,3 +12,14 @@ def evaluar_valor_cuota(probabilidad_implied, cuota_real):
 
     valor_esperado = (cuota_real * probabilidad_implied) - 1
     return valor_esperado > 0
+
+def calcular_valor_apuesta(probabilidad, cuota):
+    """
+    Calcula el valor esperado de una apuesta.
+    Valor = (probabilidad * cuota) - 1
+
+    :param probabilidad: Probabilidad estimada del evento (0.0 a 1.0)
+    :param cuota: Cuota decimal ofrecida por la casa
+    :return: Valor esperado (float), puede ser positivo o negativo
+    """
+    return (probabilidad * cuota) - 1
