5b35f33a91b1b3f6ba27535ab054ab61379744d7
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-05-04 10:00:33 -0600

    Delete pick_manual_vip.py

--- pick_manual_vip.py
+++
@@ -1,8 +0,0 @@
-# pick_manual_vip.py
-
-from utils.telegram import log_envío
-
-mensaje = "💣 PICK MANUAL DG PICKS 💣\n\n🧠 Volkanovski gana por KO/TKO a López (UFC)\n🔥 Stake: 2/10\n\n📢 ¡Solo para valientes, esto se va a cobrar!"
-
-log_envío('vip', mensaje)
-
