3d286ba9b45395e4684058ed878b2dbfb55d71e8
Rapperloco17 <dr.davidlagunes@gmail.com>
2025-06-07 12:06:33 -0600

    Delete utils directory

--- utils/leagues.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def cargar_ligas_permitidas():
-    with open("utils/leagues_whitelist_ids.json", "r") as f:
-        data = json.load(f)
-        return data  # Ya es una lista directamente
